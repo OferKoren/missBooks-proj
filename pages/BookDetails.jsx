e2533b6448fe6b0486be87d5123f33710e1ac181
OferKoren <oferkoren2@gmail.com>
2024-10-12 10:32:16 +0300

    Improve book details

--- pages/BookDetails.jsx
+++ pages/BookDetails.jsx
@@ -1,11 +1,11 @@
 export function BookDetails({ book, onEdit, onBack }) {
     console.log(book)
 
-    function getBookDetail(title, content) {
+    function getBookDetail(title, content, remark = '') {
         return (
             <div className="book-detaill">
                 <span className="detail-title">{title}:</span>
-                <span className="detail-content">{content}</span>
+                <span className="detail-content">{`${content}  ${remark}`}</span>
             </div>
         )
     }
@@ -19,18 +19,48 @@ export function BookDetails({ book, onEdit, onBack }) {
                 return 'English'
         }
     }
+
+    function getPageCountRemark() {
+        const { pageCount } = book
+        if (pageCount > 500) return 'Serious Reading'
+        if (pageCount > 200) return 'Descent Reading'
+        return 'Light Reading'
+    }
+
+    function getPublishedDateRemark() {
+        const { publishedDate } = book
+        const now = new Date()
+        if (now.getFullYear() - publishedDate > 10) return 'Vintage'
+        if (now.getFullYear() - publishedDate < 1) return 'New'
+        return ''
+    }
+
+    function getPriceClass() {
+        const { listPrice } = book
+        if (listPrice.amount > 150) return 'high-price'
+        if (listPrice.amount < 20) return 'low-price'
+        return ''
+    }
     return (
         <section className="book-details">
             <h2>{book.title}</h2>
             <h3>{book.subtitle}</h3>
-            <img src={book.thumbnail} alt="" />
+            <div className="img-wrapper">
+                <img src={book.thumbnail} alt="" />
+                {book.listPrice.isOnSale && <div className="on-sale">On Sale!</div>}
+            </div>
 
-            {getBookDetail('Year Published', book.publishedDate)}
+            {getBookDetail('Year Published', book.publishedDate, getPublishedDateRemark())}
             {getBookDetail('Author', book.authors.join(','))}
             {getBookDetail('Language', getBookLng(book.language))}
             {getBookDetail('Categories', book.categories.join(','))}
-            {getBookDetail('Pages', book.pageCount)}
-            {getBookDetail('Price', `${book.listPrice.amount} ${book.listPrice.currencyCode}`)}
+            {getBookDetail('Pages', book.pageCount, getPageCountRemark())}
+
+            <div className="book-detaill">
+                <span className="detail-title">Price:</span>
+                <span className={`detail-content ${getPriceClass()}`}>{`${book.listPrice.amount} ${book.listPrice.currencyCode}`}</span>
+            </div>
+
             <button onClick={onBack}>back</button>
             <button onClick={onEdit}>edit</button>
             <div className="book-detaill">
